f8912b68fd2cbb1e8d6ff35c625eeb4c5188bedf
GabrielRuizMiller02 <barneyruizitos@gmail.com>
2026-05-16 00:44:24 -0600

    Gabo Primero

---
+++ Assets/Scripts/Cat_Inputs.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Cat_Inputs : MonoBehaviour
+{
+
+    private PlayerInput playerInput;
+
+    private Cat_Movement catMovementScr;
+
+
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        catMovementScr = GetComponent<Cat_Movement>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 direction2D = playerInput.actions["Movement"].ReadValue<Vector2>();
+
+        catMovementScr.direction2D = direction2D;
+    }
+
+    private void OnJumpClimb()
+    {
+        catMovementScr.Jump();
+    }
+}
